99aba888bfd660325256b36f7a9f774ca08c7e10
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-05-28 16:47:02 +0200

    remove Method

--- Building Tree/first/index.js
+++ Building Tree/first/index.js
@@ -8,11 +8,19 @@ class Node {
   add(data) {
     this.children.push(new Node(data));
   }
+
+  remove(data) {
+    this.children =   this.children.filter((node) => node.data !== data);
+  }
 }
 
 const nodeOne = new Node(100);
 nodeOne.add(33);
 nodeOne.add(11);
 nodeOne.add(22);
+console.log(nodeOne);
+
+
+nodeOne.remove(33);
 
 console.log(nodeOne);
