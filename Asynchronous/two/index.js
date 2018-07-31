a7cd3543411146bb22a19e5cd48d31774099037c
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-07-31 05:13:48 +0200

    async

--- Asynchronous/two/index.js
+++ Asynchronous/two/index.js
@@ -1,15 +1,5 @@
 fetch(`https://api.github.com/users/bel7aG`)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    const dataArray = Object.values(data).map(item => document.write(`<p>${item}</p>`));
   });
-
-  const a = {
-    a: 1,
-    b: 22,
-    c: 'done',
-    d: "DONE"
-  };
-
-const b = Object.values(a).filter((_, index) => (index !== 1 && _ !== 1) && _ !== "done".toUpperCase());
-console.log(b);
