5a4f424955758d28e950a89e8e3a2564d4e2f5da
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-01 20:55:20 +0100

    finish the first step and remember some methods

--- Regex/basics/1-step.js
+++ Regex/basics/1-step.js
@@ -1,2 +1,21 @@
-let me = /bel7aG/
-console.log(me);
+let me = /bel7aG/i; //i for case sensetive
+
+// console.log(me);
+// console.log(me.source);
+me.ex
+const execResult = me.exec('ES6/7/8 Developer bel7aG'); // if match then return array with
+console.log(execResult);
+console.log(execResult[0]);
+console.log(execResult.index);
+console.log(execResult.input);
+
+      /* test return true or false */
+const testResult = me.test('bEl7AG'); // true else return false and i used above is case sensetive
+console.log(testResult);
+
+
+    /* and now the important one match() ==> return array or null */
+
+const myName = 'Belhassen Gharsallah bel7aG';
+const matchResult = myName.match(me);
+console.log(matchResult); //output same thing but the difference is the match method have the expression as arg
