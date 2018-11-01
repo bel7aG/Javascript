cdba9facd0f6f6285c73eb9105c53a865848b130
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-01 22:43:20 +0100

    play with regex

--- Regex/basics/02-step-methacaractersSymbole.js
+++ Regex/basics/02-step-methacaractersSymbole.js
@@ -5,7 +5,7 @@ const regexTest = (regex, str) => {
 
 let regex;
 
-myString = `love ecmascript and react`;
+let myString = `love ecmascript and react`;
 
 regex = /^l/;  //Must start with l
 regex = /react$/; //Must end with react
@@ -15,7 +15,6 @@ regex = /^love ecmascript and react$/; //Must be love ecmascript and react   TRU
 
 regex = /^love ecmascript and reac+$/; //not much because + 1 or more and it should be 1  FALSE
 regex = /^love ecmascript and reac*$/; //not much because * 0 or more and it should be 1  FALSE
-regex = /^love ecmascript and reac?$/; //not much because ? 0 or 1 and it should be 1      FALSE
 
 regex = /^love ecmascript and reac+/; //much because + 1 or more and it should be 1 NOW TRUE because i remove the $
 regex = /^love ecmascript and reac*/; //much because * 0 or more and it should be 1 NOW TRUE
@@ -27,4 +26,24 @@ regex = /^love ecmascript and reac[t]/; //much
 regex = /^love ecmascript and reac[^a]$/; // much
 regex = /^love ecmascript and reac[^t]$/; // not much because it's react with a bigbigbig (t)
 
+regex = /^love ecmascript and react?$/; // !IMPORTANT much because ? 0 or 1 and so t can be 1 so its much
 regexTest(regex, myString);
+
+
+
+console.log("\n\n\n\n");
+
+                         // NEXT
+myString = `bel7aG`;
+
+regex = /^b.{4}g$/i;     //matched
+regex = /.{6}/;         //matched
+regex = /^b.{5}/;      //matched
+regex = /G$/;         //matched
+regex = / *G$/;       //matched
+regex = /.?$/;        //matched
+regex = /b?e?l?7a?G$/; //matched but if i remove the 7 or i dont end with big G it will not matched again ..
+
+regexTest(regex, myString)
+
+console.log("\n\n\n\n");
